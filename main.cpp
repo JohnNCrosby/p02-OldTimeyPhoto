3021c6f806da60f187f687893ee083ed65d7e3c5
JohnNCrosby <jcrosby7@mail.csuchico.edu>
2017-10-02 16:34:02 -0700

    Added some comments before class as a pre-stepbefore class

---
+++ main.cpp
@@ -0,0 +1,41 @@
+//Author: John Crosby
+#include<vector>
+#include<string>
+#include<iostream>
+#include"bitmap.h"
+
+using namespace std;
+
+
+int main()
+{
+    //Asking the user for input on what picture they want in grayscale and make sure that the input is actually a
+    //picture. The program will keep asking the user for a picture as long as they do not input an image with the proper
+    //requirements.
+
+
+
+
+    //The program will repeat row by row, collumn by collumn to change the color gradiant of the pixel to a 
+    //greyscale version of the origanal color by taking the average of the three colors then changing the color to that
+    //grey. This will go on as long as the image has every pixel is replaced
+
+
+
+
+
+
+    //The program will then save the new picture under a different name called oldtime.bmp
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
